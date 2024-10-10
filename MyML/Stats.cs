91110de19c05c5ac596fa5a69395b5ea0c724ba8
CyberFicus <cyberficus9997@gmail.com>
2024-10-10 21:11:45 +0300

    Матрица результатов и класс для сбора статистики

---
+++ MyML/Stats.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyML
+{
+    public class Stats
+    {
+        public int Size { get; private set; }
+
+        double[] Accuracy { get; set; }
+        double[] Precision { get; set; }
+        double[] Recall { get; set; }
+        double Loss { get; set; }
+
+        public Stats(ConfusionMatrix cn, double logLossSum)
+        {
+            Size = cn.Size;
+            double N = cn.N();
+            Loss = logLossSum / N;
+            Accuracy = new double[Size];
+            Precision = new double[Size];
+            Recall = new double[Size];
+
+            for (int i = 0; i < Size; i++)
+            {
+                double TP = cn.TP(i), TN = cn.TN(i), FP = cn.FP(i), FN = cn.FN(i);
+
+                Accuracy[i] = (TP + TN) / N ;
+                Precision[i] = TP / (TP + FP);
+                Recall[i] = TP / (TP + FN);
+            }
+        }
+    }
+}
